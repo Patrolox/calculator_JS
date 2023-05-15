f8c569d5807b279611cabf6fd58c66ac754a62a7
Patrolox <122224067+Patrolox@users.noreply.github.com>
2023-05-15 23:24:57 +0200

    Add backspace and keyboard support

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 let num1 = null;
 let num2 = null;
 let operator = null;
-let displayValue = "";
+let displayValue = 0;
 let newNumber = true; // flag to indicate if the user is entering a new number
 let isCalculated = false;
 
@@ -17,10 +17,12 @@ function updateDisplay(value) {
     isCalculated = false;
     displayValue = "";
   }
+
   if (newNumber) {
     displayValue = value;
     newNumber = false;
   } else {
+    if (displayValue.length >= 9) return; //checks if the number exceeds nine digits
     displayValue += value;
   }
   display.value = displayValue;
@@ -33,17 +35,29 @@ function decimalClicked() {
   display.value = displayValue;
 }
 
+function toggleSign() {
+  if (displayValue !== "") {
+    if (operator === null) {
+      num1 = -1 * parseFloat(displayValue);
+      displayValue = num1.toString();
+    } else {
+      num2 = -1 * parseFloat(displayValue);
+      displayValue = num2.toString();
+    }
+    display.value = displayValue;
+  }
+}
+
 function operatorClicked(op) {
   if (num1 === null) {
     num1 = displayValue;
     operator = op;
-    newNumber = true; // reset the flag
   } else {
     num2 = displayValue;
     calculate();
     operator = op;
-    newNumber = true; // reset the flag
   }
+  newNumber = true; // reset the flag
 }
 
 function clearDisplay() {
@@ -89,15 +103,12 @@ function calculate() {
   switch (operator) {
     case "+":
       result = parseFloat(num1) + parseFloat(num2);
-      console.log("result: " + result);
       break;
     case "-":
       result = parseFloat(num1) - parseFloat(num2);
-      console.log("result: " + result);
       break;
     case "*":
       result = parseFloat(num1) * parseFloat(num2);
-      console.log("result: " + result);
       break;
     case "/":
       if (num2 === "0") {
@@ -105,7 +116,6 @@ function calculate() {
       } else {
         result = parseFloat(num1) / parseFloat(num2);
       }
-      console.log("result: " + result);
       break;
     default:
       result = NaN;
@@ -114,7 +124,21 @@ function calculate() {
   num1 = result;
   //num2 = null;
   operator = null;
-  displayValue = result.toString();
+  console.log("result: " + result);
+
+  if (result > 999999999) {
+    display.value = "Overflow";
+    console.log("Overflow");
+    num1 = num2 = result = displayValue = null;
+    return;
+  }
+  console.log("numberOfDigits " + ("" + result).length);
+
+  if (("" + result).length > 9) {
+    //converts result to a string and counts characters including decimal point
+    displayValue = result.toString().slice(0, 10);
+    console.log("displayValue " + displayValue);
+  } else displayValue = result.toString();
 
   display.style.opacity = 0;
   setTimeout(function () {
